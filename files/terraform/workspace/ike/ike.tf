0152f3cf19eb807b7e356d4497526b470063a848
Calvin Remsburg <cremsburg.dev@gmail.com>
2022-03-16 15:13:36 +0000

    variablization

--- files/terraform/workspace/ike/ike.tf
+++ files/terraform/workspace/ike/ike.tf
@@ -8,24 +8,24 @@ terraform {
 }
 
 provider "junos-ike" {
-    host     = "192.168.105.196"
-    port     = 22
-    username = "terraform"
-    password = "juniper123"
-    sshkey   = ""
+  host     = var.juniper_host_name
+  port     = var.juniper_ssh_port
+  sshkey   = var.juniper_ssh_key
+  username = var.juniper_user_name
+  password = var.juniper_user_password
 }
 
 module "enterprise-fw1" {
-   source     = "./enterprise-fw1"
-   providers  = { junos-ike = junos-ike }
-   depends_on = [junos-ike_destroycommit.commit-main]
+  source     = "./enterprise-fw1"
+  providers  = { junos-ike = junos-ike }
+  depends_on = [junos-ike_destroycommit.commit-main]
 }
 
 resource "junos-ike_commit" "commit-main" {
-    resource_name = "commit"
-    depends_on    = [module.enterprise-fw1]
+  resource_name = "commit"
+  depends_on    = [module.enterprise-fw1]
 }
 
 resource "junos-ike_destroycommit" "commit-main" {
-    resource_name = "destroycommit"
+  resource_name = "destroycommit"
 }
